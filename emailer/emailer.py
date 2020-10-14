fa892393a492d04b689f570361bab375a64db77c
Jennifer Lu <49915445+jennifer-lu@users.noreply.github.com>
2020-10-14 19:37:34 -0400

    Updated emailer to convert tex to pdf

--- emailer/emailer.py
+++ emailer/emailer.py
@@ -1,10 +1,13 @@
 import email, smtplib, ssl, os, subprocess
 
-
 from email import encoders
 from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+
+p = subprocess.Popen(["pdflatex", "test.tex"])
+
+p.wait()
 
 subject = "Scanner: Your File is Ready!"
 body = "Please find attatched your scanned image as a LaTeX pdf."
@@ -20,35 +23,30 @@ message["Subject"] = subject
 # Add body to email
 message.attach(MIMEText(body, "plain"))
 
+filename = "test.pdf"
 
-    p = subprocess.popen("pdflatex file.tex")
-    p.wait()
+# Open PDF file in binary mode
+with open(filename, "rb") as attachment:
+    # Add file as application/octet-stream
+    # Email client can usually download this automatically as attachment
+    part = MIMEBase("application", "octet-stream")
+    part.set_payload(attachment.read())
 
+# Encode file in ASCII characters to send by email
+encoders.encode_base64(part)
 
-    filename = "files/test.pdf"
+# Add header as key/value pair to attachment part
+part.add_header(
+    "Content-Disposition",
+    f"attachment; filename= {filename}",
+)
 
-    # Open PDF file in binary mode
-    with open(filename, "rb") as attachment:
-        # Add file as application/octet-stream
-        # Email client can usually download this automatically as attachment
-        part = MIMEBase("application", "octet-stream")
-        part.set_payload(attachment.read())
+# Add attachment to message and convert message to string
+message.attach(part)
+text = message.as_string()
 
-    # Encode file in ASCII characters to send by email
-    encoders.encode_base64(part)
-
-    # Add header as key/value pair to attachment part
-    part.add_header(
-        "Content-Disposition",
-        f"attachment; filename= {filename}",
-    )
-
-    # Add attachment to message and convert message to string
-    message.attach(part)
-    text = message.as_string()
-
-    # Log in to server using secure context and send email
-    context = ssl.create_default_context()
-    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
-        server.login(sender_email, "j33Lh8fTdhtcRJL")
-        server.sendmail(sender_email, receiver_email, text)
+# Log in to server using secure context and send email
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+    server.login(sender_email, "j33Lh8fTdhtcRJL")
+    server.sendmail(sender_email, receiver_email, text)
