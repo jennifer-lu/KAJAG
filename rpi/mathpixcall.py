d8d7f7f821511f53042ece84db36b9d320ec7aff
xaete <alice.liu@uwaterloo.ca>
2020-10-15 16:40:20 -0400

    fixed bugs

--- rpi/mathpixcall.py
+++ rpi/mathpixcall.py
@@ -4,22 +4,40 @@ import base64
 import requests
 import json
 import argparse
+import cv2
+from datetime import datetime
+
+def imgToTex(imgpath):
+    file_path = imgpath
+    image_uri = "data:image/png;base64," + base64.b64encode(open(file_path, "rb").read()).decode()
+    r = requests.post("https://api.mathpix.com/v3/text",
+        data=json.dumps({'src': image_uri}),
+        headers={"app_id": "alice_liu_uwaterloo_ca_be1dfb", "app_key": "d27cbddb366496568ec3",
+                 "Content-type": "application/json"})
+# print(json.dumps(json.loads(r.text), indent=4, sort_keys=True))
+# print(r.text)
+
+    ansJson = json.loads(r.text)
+    texResult = ""
+    try:
+        texResult = "$" + ansJson["latex_styled"] + "$"
+    except:
+        try:
+            texResult = ansJson["text"]
+        except:
+            return 0
+    texResult += "\n"
+    texHeader = '\\documentclass{article}\n\\usepackage{amsfonts,amsmath,amssymb,enumerate}\n\\usepackage{mathtools}\n\n\\begin{document}\n'
+    texFooter = '\\end{document}'
+    finalpath = "./tex/" + datetime.now().isoformat() + ".tex"
+    with open(finalpath, "w") as texOut:
+        texOut.writelines([texHeader, texResult, texFooter])
+    return finalpath
+
 
 parser = argparse.ArgumentParser(description = "(hopefully) returns LaTeX of the photo you send in")
 parser.add_argument("file_path", type=str)
 args = parser.parse_args()
 # put desired file path here
 file_path = args.file_path
-image_uri = "data:image/png;base64," + base64.b64encode(open(file_path, "rb").read()).decode()
-r = requests.post("https://api.mathpix.com/v3/text",
-    data=json.dumps({'src': image_uri}),
-    headers={"app_id": "alice_liu_uwaterloo_ca_be1dfb", "app_key": "d27cbddb366496568ec3",
-             "Content-type": "application/json"})
-# print(json.dumps(json.loads(r.text), indent=4, sort_keys=True))
-# print(r.text)
-
-ansJson = json.loads(r.text)
-try:
-    print(ansJson["latex_styled"])
-except:
-    print(ansJson["text"])
+imgToTex(file_path)
