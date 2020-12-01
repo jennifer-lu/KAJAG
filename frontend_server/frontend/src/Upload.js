7083cfde96d94abf70a4c67d7e4a1dddb97248ad
Jennifer Lu <jennifer@Jennifers-MacBook-Pro.local>
2020-11-30 21:19:44 -0500

    Upload preview

--- frontend_server/frontend/src/Upload.js
+++ frontend_server/frontend/src/Upload.js
@@ -1,39 +1,158 @@
-import React from "react";
+import React, { createRef } from "react";
+import ReactDOM from "react-dom";
 
-/**
- * Component to handle file upload. Works for image
- * uploads, but can be edited to work for any file.
- */
-function Upload() {
-  // State to store uploaded file
-  const [file, setFile] = React.useState("");
+import "./Upload.css";
 
-  // Handles file upload event and updates state
-  function handleUpload(event) {
-    setFile(event.target.files[0]);
+class Upload extends React.Component {
+  constructor(props) {
+    super(props);
 
-    // Add code here to upload file to server
-    // ...
+    this.extractFormData = this.extractFormData.bind(this);
+    this.onChangeFile = this.onChangeFile.bind(this);
+
+    this.imgPreviewImag = createRef();
   }
 
-  return (
-    <div id="upload-box">
-      <input type="file" onChange={handleUpload} />
-      <p>Filename: {file.name}</p>
-      <p>File type: {file.type}</p>
-      <p>File size: {file.size} bytes</p>
-      {file && <ImageThumb image={file} />}
-    </div>
-  );
+  extractFormData = function(form) {
+    const formData = new FormData(document.querySelector(form));
+    let values = {};
+
+    for (var pair of formData.entries()) {
+      if (values[pair[0]]) {
+        if (!(values[pair[0]] instanceof Array)) {
+          values[pair[0]] = new Array(values[pair[0]]);
+        }
+        values[pair[0]].push(pair[1]);
+      } else {
+        values[pair[0]] = pair[1];
+      }
+    }
+    return values;
+  };
+
+  generatePreviewData = file => {
+    const fr = new FileReader();
+    return new Promise((resolve, reject) => {
+      fr.addEventListener("load", e => {
+        const div = document.createElement("div");
+        const img = document.createElement("img");
+        img.src = fr.result;
+        img.setAttribute("class", "preview");
+        div.appendChild(img);
+        resolve(div);
+      });
+      fr.addEventListener("error", e => {
+        reject();
+      });
+      fr.readAsDataURL(file);
+    });
+  };
+
+  // saveImage = function(file, filename, ref, callbacks) {
+  //   if (!ref) ref = firebase.storage().ref();
+  //   if (!callbacks) callbacks = {};
+  //   if (mimes[file.type].extensions[0]) {
+  //     callbacks.success = callbacks.success || console.log;
+  //     callbacks.progress = callbacks.progress || console.log;
+  //     callbacks.error = callbacks.error || console.error;
+
+  //     // Create the file metadata
+  //     var metadata = {
+  //       contentType: file.type
+  //     };
+
+  //     // Upload file and metadata to the object
+  //     var uploadTask = ref
+  //       .child(filename + "." + mimes[file.type].extensions[0])
+  //       .put(file, metadata);
+  //     uploadTask.on(
+  //       "state_changed",
+  //       callbacks.progress,
+  //       callbacks.error,
+  //       callbacks.success
+  //     );
+
+  //     return uploadTask.then(function(snapshot) {
+  //       return snapshot.ref.getDownloadURL();
+  //     });
+  //   }
+  // };
+
+  // sendData = (text, files) =>
+  //   Promise.all(
+  //     files.map(file =>
+  //       this.saveImage(
+  //         file,
+  //         +new Date() + "_" + Math.random(),
+  //         allImagesRef,
+  //         progress
+  //       )
+  //     )
+  //   ).then(values =>
+  //     userImagesRef.child(user.uid).push({
+  //       status: text,
+  //       pictures: values,
+  //       timestamp: +new Date()
+  //     })
+  //   );
+
+  removeAllChildren = el => {
+    while (el.childElementCount) {
+      el.removeChild(el.children[0]);
+    }
+  };
+
+  renderCollection = (collection, container) => {
+    this.removeAllChildren(container);
+    Promise.all(collection.map(this.generatePreviewData)).then(imgs =>
+      imgs.map((img, i) => {
+        img.setAttribute("index", i);
+        img.addEventListener("click", e => {
+          collection.splice(i, 1);
+          this.renderCollection(collection, container);
+        });
+        container.appendChild(img);
+      })
+    );
+  };
+
+  onChangeFile(e) {
+    let fileCollection = [];
+
+    const formData = this.extractFormData("#my-form");
+    console.log(formData);
+
+    while (fileCollection.length) {
+      fileCollection.pop();
+    }
+
+    // [].slice.call(formData.pictures).map(f => fileCollection.push(f));
+    Array.from(e.target.files).map(f => fileCollection.push(f));
+    this.renderCollection(fileCollection, this.imgPreviewImag.current);
+  }
+
+  render() {
+    return (
+      <div>
+        <center>
+          <form action="" className="myForm" id="my-form">
+            <label htmlFor="pictures">Upload Files</label>
+            <input
+              name="pictures"
+              type="file"
+              id="pictures"
+              accept="image/*"
+              multiple
+              onChange={this.onChangeFile}
+            />
+          </form>
+
+          <div ref={this.imgPreviewImag} />
+        </center>
+      </div>
+    );
+  }
 }
-
-/**
- * Component to display thumbnail of image.
- */
-const ImageThumb = ({ image }) => {
-  return <img src={URL.createObjectURL(image)} alt={image.name} />;
-};
-
 
 export default function App() {
   return <Upload />;
