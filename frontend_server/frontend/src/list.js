41aa075b5ec215d9715868f59a61113689062899
Golden-Wang <golden3.1415926@gmail.com>
2020-12-06 14:13:10 -0800

    moved code here from another file

--- frontend_server/frontend/src/list.js
+++ frontend_server/frontend/src/list.js
@@ -1,3 +1,21 @@
+import axios from "axios"
 import Cookies from 'universal-cookie';
 const cookies = new Cookies();
 
+
+export default function PostRequest() {
+    current_session = cookie.get("session")
+
+    axios.post('http://localhost:9001/list', {
+        session: current_session
+    })
+    .then((response) => {
+        console.log(response);
+
+        // do somthing here, still need help displaying the filenames
+        alert("it worked")
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+}
